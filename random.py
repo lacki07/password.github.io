7ee4a5d069f346c37ee097d0fda9b7a9cac332bc
bhargav k c <151606544+lacki07@users.noreply.github.com>
2023-12-03 18:01:09 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,13 @@
+import random
+
+lower= "abcdefghijklmnopqrstuvwxyz"
+upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers="0123456789"
+symbols="!@#$%^&*()."
+
+string = lower + upper + numbers + symbols
+length = 16
+password ="".join(random.sample(string,length))
+
+print("Your new password IS:"+ password)
+
